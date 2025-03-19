e7faf30343914005b66f91ec655d23b2f90ade30
AllanPMedeiros <allanramospatricio1267@Gmail.com>
2025-03-19 13:10:28 -0300

    interações com a tela de usuario

--- js/server.js
+++ js/server.js
@@ -73,6 +73,69 @@ app.get('/test-connection', (req, res) => {
   });
 });
 
+app.get('/user-info', (req, res) => {
+  const userId = req.query.userId; // Supondo que o ID do usuário seja passado como query param
+  const query = 'SELECT tb_nome AS nome, tb_email AS email, tb_setor AS setor FROM tb_usuarios WHERE tb_id = ?';
+
+  console.log('userId:', userId);
+
+  connection.query(query, [userId], (err, results) => {
+    if (err) {
+      console.error('Erro ao buscar informações do usuário:', err);
+      res.status(500).send('Erro ao buscar informações do usuário.');
+      return;
+    }
+    if (results.length > 0) {
+      console.log('Informações do usuário:', results[0]);
+      res.status(200).json(results[0]);
+    } else {
+      res.status(404).send('Usuário não encontrado.');
+    }
+  });
+});
+
+app.post('/update-user', (req, res) => {
+  const { nome, email, setor, senha } = req.body;
+  const query = 'UPDATE tb_usuarios SET tb_nome = ?, tb_setor = ?, tb_senha = ? WHERE tb_email = ?';
+
+  connection.query(query, [nome, setor, senha, email], (err, results) => {
+    if (err) {
+      console.error('Erro ao atualizar usuário:', err);
+      res.status(500).send('Erro ao atualizar usuário.');
+      return;
+    }
+    res.status(200).send('Usuário atualizado com sucesso.');
+  });
+});
+
+app.post('/add-user', (req, res) => {
+  const { nome, email, setor, senha } = req.body;
+  const query = 'INSERT INTO tb_usuarios (tb_nome, tb_email, tb_setor, tb_senha) VALUES (?, ?, ?, ?)';
+
+  connection.query(query, [nome, email, setor, senha], (err, results) => {
+    if (err) {
+      console.error('Erro ao adicionar usuário:', err);
+      res.status(500).send('Erro ao adicionar usuário.');
+      return;
+    }
+    res.status(200).send('Usuário adicionado com sucesso.');
+  });
+});
+
+app.post('/delete-user', (req, res) => {
+  const { email } = req.body;
+  const query = 'DELETE FROM tb_usuarios WHERE tb_email = ?';
+
+  connection.query(query, [email], (err, results) => {
+    if (err) {
+      console.error('Erro ao excluir usuário:', err);
+      res.status(500).send('Erro ao excluir usuário.');
+      return;
+    }
+    res.status(200).send('Usuário excluído com sucesso.');
+  });
+});
+
 app.listen(port, () => {
   console.log('Servidor rodando na porta 3000.');
 });
